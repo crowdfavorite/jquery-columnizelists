f9fa147fa3a99b381d3a2ae2dc9f813e06f28e6f
gbrander <gbrander@546ca710-db30-0410-8887-d9dd8e66aff0>
2010-01-29 18:21:27 +0000

    Fixed columnize lists so it works with more than one passed element. Also extended args, jQuery plugin style for defaults
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/jquery-plugins/columnizelists/trunk@20540 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ jquery.columnizelists.js
@@ -0,0 +1,69 @@
+/**
+ * ColumnizeLists
+ * @version 0.1
+ * @requires jQuery
+ * Copyright 2010, Crowd Favorite
+ *
+ * Break lists into chunks so you can build top-to-bottom-left-to-right columned lists
+ * Usage: $('ul.my-columnizer-class').columnizeLists({});
+ */
+;(function($) {
+
+	$.fn.columnizeLists = function(args) {
+		// Merge our default args and the user's args
+		var args = $.extend({}, $.fn.columnizeLists.args, args);
+
+		// Loop through every target
+		this.each(function(){
+			var $this = $(this);
+			
+			var cont = $this.parent();
+			var items = $this.find('li');
+			var items_count = items.size();
+
+			// If we have the column preference, figure out how many rows we should have, then do rows
+			if (args.pref == 'cols') {
+				var rem = items_count % args.cols;
+				args.rows = Math.floor(items_count / args.cols);
+				rem ? extra = 1 : extra = 0;
+			}
+
+			// Put a wrapper around our new divs we're creating
+			cont.append('<div class="' + args.divWrapperClass + '"></div>');
+			var div_wrapper = cont.find("." + args.divWrapperClass);
+
+			// Loop through each list item
+			var i = 0;
+			var col_num = 0;
+			items.each(function() {
+				// fancy-pants math to see if we should append an extra row till we have no remainder 
+				(extra && col_num <= rem) ? row_count = args.rows + extra : row_count = args.rows;
+				if (i % row_count == 0) {
+					col_num++;
+					i = 0;
+					cur_col = 'col-' + col_num.toString();
+					div_wrapper.append('<div class="' + args.colClass + ' ' + cur_col + '"><ul></ul></div>');
+				}
+
+				$(this).appendTo(div_wrapper.find('.' + cur_col + ' ul'));
+				i++;
+			});
+
+			// Now add the container class, and remove the initial ul
+			cont.addClass(args.containerClass).children("ul").remove();
+		});
+		
+	};
+	
+	/**
+	 * Default settings
+	 */
+	$.fn.columnizeLists.args = {
+		pref: 'cols',
+		rows: 10,
+		cols: 4,
+		containerClass: 'clearfix',
+		colClass: 'cf-col',
+		divWrapperClass: 'div-wrapper'
+	}
+})(jQuery);
